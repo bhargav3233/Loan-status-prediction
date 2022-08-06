b64f6e36e43c65b323c6c5feafc6d7dfc3228e29
bhargav3233 <109935418+bhargav3233@users.noreply.github.com>
2022-08-06 19:05:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 16:59:14 2022
+
+@author: Bhargav
+"""
+
+import pickle
+import streamlit as st
+ 
+# loading the trained model
+pickle_in = open('D:/Loan/classifier.pkl', 'rb') 
+classifier = pickle.load(pickle_in)
+ 
+@st.cache()
+  
+# defining the function which will make the prediction using the data which the user inputs 
+def prediction(Gender, Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area):   
+ 
+    # Pre-processing user input    
+    if Gender == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+ 
+    if Married == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+        
+    if Dependents == 0:
+        Dependents=0
+    elif Dependents==1:
+        Dependents=1
+    elif Dependents ==2:
+        Dependents=2
+    else:
+        Dependents = 3
+    
+    if Education =='Graduate':
+        Education = 1
+    else:
+        Education=0
+    
+    if Self_Employed == 'Employed':
+        Self_Employed = 1
+    else:
+        Self_Employed=0
+        
+        
+    ApplicantIncome = ApplicantIncome/1000
+    CoapplicantIncome = CoapplicantIncome/1000
+    LoanAmount = LoanAmount / 1000
+    Loan_Amount_Term = Loan_Amount_Term/12
+    
+    
+    
+    if Credit_History == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1  
+
+    if Property_Area == 'Semiurban':
+        Property_Area=0
+    elif Property_Area =='Urban':
+        Property_Area=1
+    else:
+        Property_Area=2
+
+
+ 
+    # Making predictions 
+    prediction = classifier.predict([[Gender, Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area]])
+     
+    if (prediction == 0):
+        pred = 'Approved'
+    else:
+        pred = 'Rejected'
+    return pred
+      
+  
+# this is the main function in which we define our webpage  
+def main():       
+    # front end elements of the web page 
+    html_temp = """ 
+    <div style ="background-color:yellow;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
+    </div> 
+    """
+      
+    # display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html = True) 
+      
+    # following lines create boxes in which user can enter data required to make prediction 
+    Gender = st.selectbox('Gender',("Male","Female"))
+    Married = st.selectbox('Marital Status',("Unmarried","Married"))
+    Dependents = st.selectbox('Dependents',(0,1,2,3))
+    Education = st.selectbox('Education',("Graduate","Not Graduate"))
+    Self_Employed = st.selectbox('Self_Employed',("Employed","Un employed"))
+    ApplicantIncome = st.number_input("Applicant monthly income")
+    CoapplicantIncome = st.number_input("Co Applicant monthly income") 
+    LoanAmount = st.number_input("Total loan amount")
+    Loan_Amount_Term= st.number_input('Enter Loan Term')
+    Credit_History = st.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
+    Property_Area = st.selectbox('Property_Area',("Semiurban","Urban","Rural"))  
+    result = ""
+      
+    # when 'Predict' is clicked, make the prediction and store it 
+    if st.button("Predict"): 
+        result = prediction(Gender, Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area) 
+        st.success(result)
+     
+if __name__=='__main__': 
+    main()
+    
+
+#('Your loan is {}'.format(result))print(LoanAmount)
